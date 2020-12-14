c0c0b212a4fb582dc029034791e9944e1fdbb660
skomorow <46261056+skomor@users.noreply.github.com>
2020-12-14 12:17:20 +0100

    przed frontem dla usera

--- Source/Spirala.Web/ClientApp/src/App.js
+++ Source/Spirala.Web/ClientApp/src/App.js
@@ -14,23 +14,42 @@ import SoldierList from "./components/SoldierList";
 import FamilyMembers from "./components/FamilyMembers";
 import MilitaryUnits from "./components/MilitaryUnits";
 import Vehicles from "./components/Vehicles";
+import authService from "./components/api-authorization/AuthorizeService";
 
 export default class App extends Component {
-  static displayName = App.name;
-
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <AuthorizeRoute path='/fetch-data' component={FetchData} />
-            <AuthorizeRoute path='/ListUsers' component={ListUsers} />
-            <Route path='/SoldierList' component={SoldierList} />
-            <Route path='/FamilyMembers' component={FamilyMembers} />
-            <Route path='/MilitaryUnits' component={MilitaryUnits} />
-            <Route path='/VehicleList' component={Vehicles} />
-        <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
-      </Layout>
-    );
-  }
+    static displayName = App.name;
+    
+    constructor(props) {
+        super(props);
+        this.state={
+            role:"",
+            isAuthenticated:false
+        }
+    }
+    componentDidMount() {
+        this._subscription = authService.subscribe(() => this.populateState());
+        this.populateState();
+    }
+    async populateState() {
+        const [isAuthenticated, user] = await Promise.all([authService.isAuthenticated(), authService.getUser()])
+        this.setState({
+            isAuthenticated,
+            role: user && user.role
+        });
+    }
+    render() {
+        return (
+            <Layout>
+                <Route exact path='/' component={() => <Home passedProp = {this.state.role}/>}/>
+                <Route path='/counter' component={Counter}/>
+                <AuthorizeRoute path='/fetch-data' component={() => <FetchData passedProp = {this.state.role}/>}/>
+                <AuthorizeRoute path='/ListUsers' component={() => <ListUsers passedProp = {this.state.role} />}/>
+                <Route path='/SoldierList' component={() => <SoldierList passedProp = {this.state.role}/>}/>
+                <Route path='/FamilyMembers' component={FamilyMembers}/>
+                <Route path='/MilitaryUnits' component={MilitaryUnits}/>
+                <Route path='/VehicleList' component={Vehicles}/>
+                <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes}/>
+            </Layout>
+        );
+    }
 }
